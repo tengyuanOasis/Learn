801e4a99cf80a08cfbde542cf7380b836c1ca98e
June <majunteng@huize.com>
2021-12-13 17:40:04 +0800

    feat: life circle

---
+++ learn-vuex/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+	<div id="app">
+		{{ $store.state.count }}
+		<button @click="$store.commit('increment', 10)">increment</button>
+		<button @click="$store.commit('decrement', 10)">decrement</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "App"
+	};
+</script>
